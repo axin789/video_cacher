8851a81188678a4a3f6f13fce5ebeea5f7b8518d
doubledog789 <248255786+doubledog789@users.noreply.github.com>
2026-07-09 22:42:12 +0700

    refactor: 删除孤立的旧下载实现，移除 sqlite/flutter_hls_parser 等 5 个未用依赖

--- lib/download/task_store/task_store.dart
+++
@@ -1,8 +0,0 @@
-import 'task_store_base.dart';
-
-// 条件导入，并统一使用 impl 别名
-import 'task_store_io.dart' if (dart.library.html) 'task_store_web.dart'
-    as impl;
-
-// 对外统一工厂，实际调用实现文件里的函数
-Future<TaskStore> openTaskStore() => impl.openTaskStore();
--- lib/download/worker/base_worker.dart
+++
@@ -1,6 +0,0 @@
-abstract class BaseWorker<TTask> {
-  TTask get task;
-  Future<void> start();
-  void pause();
-  Future<void> cancel({bool deleteFiles = false});
-}
